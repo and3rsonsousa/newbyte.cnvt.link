e6f5b16aec80f20ad2182051463551964a486245
Anderson Sousa <and3rsonsousa@outlook.com>
2022-04-19 08:48:37 -0300

    no log backdrop

--- app/components/Backdrop.tsx
+++ app/components/Backdrop.tsx
@@ -1,8 +1,6 @@
 import { BackdropType } from "~/types";
 
 export default function Backdrop({ className, file }: BackdropType) {
-	console.log(file);
-
 	return (
 		<div className={`backdrop ${className ? className : ""}`}>
 			{file && <img src={file.path} className={file.className} alt="" />}
